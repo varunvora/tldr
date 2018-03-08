372e0c32cad8270c3bb55601ab0beb934bf3ed04
vinayak <vinayakawanti@gmail.com>
2018-03-08 22:33:23 +0530

    tokenize

--- dataset_loader.py
+++ dataset_loader.py
@@ -6,15 +6,15 @@ x_master, y_master = load(data_file)
 del data_file
 
 #Verification
-print("Size of X : ", len(x_master))
-print("Size of Y : ", len(y_master))
+# print("Size of X : ", len(x_master))
+# print("Size of Y : ", len(y_master))
 
-print("Some random summaries in our dataset")
-for i in range(3) :
-	temp = randrange(len(x_master))
-	print("ARTICLE : ")
-	print(x_master[temp])
-	print("SUMMARY : ")
-	print(y_master[temp])
-	print()
+# print("Some random summaries in our dataset")
+# for i in range(3) :
+# 	temp = randrange(len(x_master))
+# 	print("ARTICLE : ")
+# 	print(x_master[temp])
+# 	print("SUMMARY : ")
+# 	print(y_master[temp])
+# 	print()
 
