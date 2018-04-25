4a149e8018e7ec817a0e3000109d6018dc247273
Varun Vora <varunvora@live.com>
2018-04-25 15:37:39 +0530

    Added normalizer

--- abstractive.py
+++ abstractive.py
@@ -1,7 +1,23 @@
 from sentence_score import extractive_summarizer
 from word_score import get_word_score
 from pickle import load
+from string import punctuation
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 import random
+porter = PorterStemmer()
+
+def normalize_text(text) :
+    text = text.lower()
+    text = text.replace("'", "") #wouldn't -> wouldnt
+    for char in list(punctuation) : #remove all punctuation
+        text = text.replace(char, " ")
+    #text = " ".join(filter(lambda x : x not in stopwords , text.split())) #remove stopwords
+    #text = " ".join(filter(lambda x : x.isalpha(), text.split())) #remove numbers
+    text = " ".join([porter.stem(x) for x in text.split()]) #stemming
+    text = " ".join(text.split()) #ensure words delimited by single space only
+    return text
+
 with open('clean_dataset.pkl', 'rb') as fp:
     article_word_list, summary_word_list = load(fp)
 
@@ -30,26 +46,34 @@ Instead, Trump flew to Moscow on a Bombardier Global 5000 private jet owned by P
 
 The jet -- tail number N443PR -- had flown from Las Vegas to Asheville on Nov. 6, according to the flight records that Bloomberg purchased from FlightAware, an aviation data company. The flight records don’t say who was aboard the jet, which took off from Asheville at 9:15 p.m. on Thursday, Nov. 7, bound for Moscow’s Vnukovo International Airport."
 """
-swap_erate=0.05
-remove_rate=0.05
-unk_rate=0.03
-lower_lim=0.3 ##starting percentage where errors should be introduced
-upper_lim=0.9 ##ending percentage after wich error should be avoided 
-summary = extractive_summarizer(article, word_score_dictionary, sentence_count = 4, log_probability = True)
-lsum=list(summary.split(" "));
-print(len(lsum))
-swapping_indexes=random.sample(range(int(0.3*len(lsum)),int(0.90*len(lsum))),int(swap_erate*len(lsum)))
-print(swapping_indexes)
-for i in swapping_indexes:
-    temp=lsum[i]
-    lsum[i]=lsum[i+1]
-    lsum[i+1]=temp
-swapping_indexes=random.sample(range(int(0.3*len(lsum)),int(0.90*len(lsum))),int(remove_rate*len(lsum)))
-for i in swapping_indexes:
-    lsum[i]=""
-swapping_indexes=random.sample(range(int(0.3*len(lsum)),int(0.90*len(lsum))),int(remove_rate*len(lsum)))
-for i in swapping_indexes:
-    lsum[i]="UNK"
+def abstractive_summarizer(article, word_score_dictionary, sentence_count = 4, log_probability = True) :
+	swap_erate=0.5
+	swap_prob = 0.05
+	remove_rate=0.1
+	unk_rate=0.05
+	lower_lim=0.1 ##starting percentage where errors should be introduced
+	upper_lim=1 ##ending percentage after wich error should be avoided 
+	summary = extractive_summarizer(article, word_score_dictionary, sentence_count = 4, log_probability = True)
+	lsum=list(summary.split(" "));
+	#print(len(lsum))
+	while random.random() < swap_prob :
+		swapping_indexes=random.sample(range(int(lower_lim*len(lsum)),int(upper_lim*len(lsum))),int(swap_erate*len(lsum)))
+	#print(swapping_indexes)
+		for i in swapping_indexes:
+			temp=lsum[i]
+			lsum[i]=lsum[i+1]
+			lsum[i+1]=temp
+	swapping_indexes=random.sample(range(int(lower_lim*len(lsum)),int(upper_lim*len(lsum))),int(remove_rate*len(lsum)))
+	for i in swapping_indexes:
+		lsum[i]=""
+	lsum=normalize_text(" ".join(lsum))
+	lsum = lsum.split()
+	swapping_indexes=random.sample(range(int(lower_lim*len(lsum)),int(upper_lim*len(lsum))),int(remove_rate*len(lsum)))
+	for i in swapping_indexes:
+		lsum[i]="__UNK__"
 
-summ=" ".join(lsum)
-print(summ)
+	
+	return " ".join(lsum)
+
+abstractive_summary = abstractive_summarizer(article, word_score_dictionary)
+print(abstractive_summary)
